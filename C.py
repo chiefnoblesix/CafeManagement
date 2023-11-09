5f9a3d65247402519b5fe1ebfb2946ea2a692c37
Bryan T <90597588+Poopedy@users.noreply.github.com>
2023-11-09 10:39:34 +0800

    Update C.py

--- C.py
+++ C.py
@@ -1,8 +1,8 @@
-from flask import render_template, request, flash, session, redirect, url_for
+from flask import render_template, request, flash, session, redirect, url_for, session
 from CafeDB import *
 from E import *
 
-
+bidID = 1
 #Controller Classes
 
 class LogInController:
@@ -18,10 +18,10 @@ class WorkSlotController:
         return WorkSlotEntity.get_available_work_slots()
 
 
-class BidsController:
+class ViewC:
     @staticmethod
-    def get_from_entity_bids():
-        return BidsEntity.get_all_bids()
+    def get_from_entity_bids(user_id):
+        return BidsEntity.get_all_bids(user_id)
     
 class CreateWorkslotC:
     def create_workslot(shift_type, date, status):
@@ -59,3 +59,59 @@ class EditAccountC:
             return StaffEntity.edit_acc(olduser, username, password, userRole, job, avail)
         else:
             return redirect(url_for('SysAdminHome'))
+        
+class DeleteAccC:
+    def delete_acc(delete_id):
+        return StaffEntity.delete_acc(delete_id)
+    
+class AdminSearchC:
+    def search(query):
+        return StaffEntity.search(query)
+            
+
+
+class DeleteBidC:
+    def delete_bid(id):
+        if BidsEntity.search_bid(id) is not None:
+
+            return BidsEntity.delete_bid(id)
+        
+class CreateBidC:
+    def get_from_entity(user_id):
+        workslots = WorkSlotEntity.get_available_work_slots()
+        bids = BidsEntity.get_all_bids(user_id)
+        for workslot in workslots:
+            for bid in bids:
+                if workslot.id == bid.shift_id:
+                    workslots.remove(workslot)
+
+        return workslots
+        
+        
+class BidC:
+    def place_the_bid(slot_id):
+        global bidID
+        workslot = WorkSlotEntity.get_workslot(slot_id)
+        if workslot:
+            bid_id = bidID
+            shift_id = workslot.id
+            shift_type = workslot.shiftType
+            shift_date = workslot.date
+            staff_user = session.get('user_id')
+            
+            bidID += 1
+            return BidsEntity.create_bid(bid_id,shift_id, shift_type, shift_date, staff_user )
+        else:
+            return redirect(url_for('create_bid'))
+        
+class UpdateBidC:
+    def get_bid_updated(id):
+        bid = BidsEntity.get_a_bid(id)
+        return bid
+    
+    def update_bid(id, shift_type, shift_date):
+        return BidsEntity.update_bid(id, shift_type, shift_date)
+            
+        
+
+
